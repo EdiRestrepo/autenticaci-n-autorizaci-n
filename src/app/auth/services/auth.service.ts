dc3f4ddb1955ab861cd58bd2b04115cdd246fc4e
EdiRestrepo <edisonestival@gmail.com>
2025-05-21 21:31:26 -0500

    first commit

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, signal } from '@angular/core';
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+@Injectable({
+  providedIn: 'root'
+})  
+export class AuthService {
+
+    private _authStatus = signal<AuthStatus>('checking');
+    
+
+}
